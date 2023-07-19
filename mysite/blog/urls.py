ecce3662ea3f14bed7936e9501b0c0a0daf0779a
Hemin Jeong <jhemin0415@gmail.com>
2023-07-19 11:12:03 +0900

    페이지네이션 뷰, 템플릿 추가

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail')
+    path('<int:year>/<int:month>/<int:day>/<str:post>/', views.post_detail, name='post_detail')
+    # path(r'?P<year>[0-9]{4})/(?P<month>[0-9]{2})/?P<day>[0-9]{2})/(?P<str:post>[\w-]+)/$/', views.post_detail, name='post_detail')
 
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 from .models import Post
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts':posts})
 
 def post_detail(request, year, month, day, post):
-    # try:
-    #     post = Post.published.get(id=id)
-    # except:
-    #     raise Http404("No Post found")
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED,
                              slug=post, publish__year=year,
                              publish__month=month,

a87949f01c296012a3b49ab4d768a11464fddfb9
Hemin Jeong <jhemin0415@gmail.com>
2023-07-19 15:50:36 +0900

    이메일 전송 기능 실습

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<str:post>/', views.post_detail, name='post_detail')
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<str:post>/', views.post_detail, name='post_detail'),
     # path(r'?P<year>[0-9]{4})/(?P<month>[0-9]{2})/?P<day>[0-9]{2})/(?P<str:post>[\w-]+)/$/', views.post_detail, name='post_detail')
-
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,19 +2,56 @@ from django.shortcuts import render
 from .models import Post
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+from django.views.generic import ListView
+from .forms import EmailPostForm
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 # Create your views here.
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/post/list.html', {'posts':posts})
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED,
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
                              slug=post, publish__year=year,
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post':post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']}님이 {post.title}을(를) 추천합니다."
+            message = f"{post.title}을(를) {post_url} 에서 읽어보세요.\n\n" \
+                      f"{cd['name']}의 의견 : {cd['comments']}"
+            send_mail(subject, message, 'jhemin0415@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post':post,
+                                                    'form':form,
+                                                    'sent':sent})
